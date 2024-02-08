313eb9f12da35110a9058541194dcdd66ee1cca6
Leon Wagner <65195784+LeWagn@users.noreply.github.com>
2024-02-08 21:31:58 +0100

    Delete directory

--- src/Analysis_Zeug/Main.java
+++
@@ -1,9 +0,0 @@
-package Analysis_Zeug;
-
-public class Main {
-    public static void main(String[] args) {
-        
-        long f = (long) Math.ceil(2 / (7 * 1));
-        System.out.println(f);
-    }
-}
